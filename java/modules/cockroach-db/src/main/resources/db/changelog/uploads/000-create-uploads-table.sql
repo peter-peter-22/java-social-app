71764c6a28014d74510b580e6941749aaf832036
Anonymous <anon@gmail.com>
2026-07-19 14:42:52 +0200

    fix database schema in the uploads table

--- java/modules/cockroach-db/src/main/resources/db/changelog/uploads/000-create-uploads-table.sql
+++ java/modules/cockroach-db/src/main/resources/db/changelog/uploads/000-create-uploads-table.sql
@@ -5,16 +5,15 @@ create table if not exists uploads (
     id uuid primary key default gen_random_uuid(),
     object_path varchar(100) not null,
     bucket varchar(32) not null,
-    created_by uuid not null ON DELETE CASCADE,
+    created_by uuid not null references users
+(
+    id
+) ON DELETE CASCADE, -- uploads_created_by_fkey
     file_type varchar(8) not null,
     created_at timestamptz not null default now(),
     status varchar(16) not null,
 
     unique (object_path, bucket)
-    constraint fk_uploads_user FOREIGN KEY created_by REFERENCES users
-(
-    id
-)
 );
 
 --rollback drop table if exists uploads;
